7c3a712e0971c8d84e092c580eb6eeb2045c02b3
R3R3R3 <>
2020-03-22 23:14:41 +0100

    Minor tweak of animation to appease set_attach

--- 3d_armor/init.lua
+++ 3d_armor/init.lua
@@ -266,7 +266,7 @@ default.player_register_model("3d_armor_character.b3d", {
 		"3d_armor_trans.png",
 	},
 	animations = {
-		stand = {x=0, y=79},
+		stand = {x=0, y=80},
 		lay = {x=162, y=166},
 		walk = {x=168, y=187},
 		mine = {x=189, y=198},
